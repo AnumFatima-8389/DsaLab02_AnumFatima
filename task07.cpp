99408c84eacf33c303c3bbcd51568a20964d8c7f
AnumFatima-8389 <anfatima.bscs24seecs@seecs.edu.pk>
2025-09-15 16:08:54 +0500

    Code added

--- task07.cpp
+++ task07.cpp
@@ -4,7 +4,6 @@ using namespace std;
 void returnIndices(vector<int> v, int key)
 {
     vector<int> result;
-    
     if (v.empty())
     {
         cout << "Array is empty\n";
@@ -13,9 +12,7 @@ void returnIndices(vector<int> v, int key)
     for (int i = 0; i < v.size(); i++)
     {
         if (v.at(i) == key)
-        {
             result.push_back(i);
-        }
     }
     if (result.empty())
     {
@@ -24,14 +21,12 @@ void returnIndices(vector<int> v, int key)
     }
     cout << key << " is present at indices:\n";
     for (int i = 0; i < result.size(); i++)
-    {
         cout << result.at(i) << " ";
-    }
 }
 int main()
 {
     cout << "\t\t-----Task07-----\n";
     vector<int> v = {1,2,2,3,1};
-    returnIndices(v, 2);
+    returnIndices(v, 0);
     return 0;
 }
