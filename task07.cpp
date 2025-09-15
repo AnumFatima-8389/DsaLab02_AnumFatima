675b89850a0dd480fbf9bcff056c03976d27c271
AnumFatima-8389 <anfatima.bscs24seecs@seecs.edu.pk>
2025-09-15 15:15:08 +0500

    Added task 07 empty file

---
+++ task07.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void returnIndices(vector<int> v, int key)
+{
+    vector<int> result;
+    
+    if (v.empty())
+    {
+        cout << "Array is empty\n";
+        return;
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v.at(i) == key)
+        {
+            result.push_back(i);
+        }
+    }
+    if (result.empty())
+    {
+        cout << "Key not found\n";
+        return;
+    }
+    cout << key << " is present at indices:\n";
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result.at(i) << " ";
+    }
+}
+int main()
+{
+    cout << "\t\t-----Task07-----\n";
+    vector<int> v = {1,2,2,3,1};
+    returnIndices(v, 2);
+    return 0;
+}
